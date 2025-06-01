dc1b47729efe94e1ce4a369f8ea3da31ae5c437c
Kishan89 <kishan895737@gmail.com>
2025-06-01 22:42:06 +0530

    Initial commit

---
+++ create-react-comp/src/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello() {
+  var myName = "kishan";
+  let fullName = () => {
+    return "kishan";
+  };
+  return (
+    <h3>Hello this is the future speaking, i am your master {fullName()}</h3>
+  );
+}
+export default Hello;
