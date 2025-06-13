ea5b5b886f9c1ce9147e737e5bff4d5b8642b668
Kishan89 <kishan895737@gmail.com>
2025-06-13 22:48:34 +0530

    update

--- 18 Fragments/learning-fragment/src/components/FoodItems.jsx
+++ 18 Fragments/learning-fragment/src/components/FoodItems.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Item from "./Item";
 function FoodItems({ items }) {
+  let [activeItems, setActiveItems] = useState([]);
+  let onBuyButton = (item, event) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
+
   return (
     <ul className="list-group">
       {items.map((item) => (
         <Item
           key={item}
           foodItem={item}
-          handleBuyButton={() => console.log(`${item} bought`)}
+          bought={activeItems.includes(item)}
+          handleBuyButton={(event) => onBuyButton(item, event)}
         />
       ))}
     </ul>
---
+++ Projects/calculatorv2/src/components/Display.jsx
@@ -0,0 +1,13 @@
+import styles from "./Display.module.css";
+function Display({ displayValue }) {
+  return (
+    <input
+      className={styles.display}
+      type="text"
+      value={displayValue}
+      readOnly
+    ></input>
+  );
+}
+
+export default Display;
